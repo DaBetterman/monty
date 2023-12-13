9a0026da5294b948f560e2f57116a7ede10c57fc
Method Mkoma <gershommethod02@gmail.com>
2023-12-13 22:01:00 +0300

    Task 3

--- swap.c
+++ swap.c
@@ -11,7 +11,7 @@ void f_swap(stack_t **head, unsigned int number)
 
 	(void)number;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", number);
 		fclose(bus.file);
