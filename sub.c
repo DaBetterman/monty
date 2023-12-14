0e680197d53d49f82cd56fb58699ad725c13686d
Method Mkoma <gershommethod02@gmail.com>
2023-12-14 19:32:03 +0300

    Task 6

---
+++ sub.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+* f_sub - Subtracts the top element of the stack from the second top element.
+* @head: Double pointer to the beginning of the stack.
+* @number: not in use
+* Return: nothing to return
+*/
+void f_sub(stack_t **head, unsigned int number)
+{
+    stack_t *temp;
+
+    (void)number;
+
+    if (*head == NULL || (*head)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't sub, stack too short\n", number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    (*head)->next->n -= (*head)->n;
+
+    temp = *head;
+    *head = (*head)->next;
+    (*head)->prev = NULL;
+
+    free(temp);
+}
+
